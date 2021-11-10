fb66a7096349f930dbf17dd2a5139262f075b39d
Lorena Silva <lorenarsdasilva@gmail.com>
2021-11-10 01:13:22 -0300

    Primeiro commit

---
+++ src/Studyng/java/Exemplo_nome_e_idade.java
@@ -0,0 +1,32 @@
+package Studyng.java;
+import java.util.Scanner;
+
+/*
+Faça um programa que leia dois valores
+o primeiro representa o nome do aluno e o outro representasua idade.
+pare o programa digitando 0 no campo nome.
+ */
+
+public class Exemplo_nome_e_idade {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while (true) {
+
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if (nome.equals("0")) break;
+
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+
+        System.out.println("Continua aqui......");
+
+    }
+}
