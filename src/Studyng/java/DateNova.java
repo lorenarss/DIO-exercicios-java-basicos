fc1a63252162fa067ad65aa0ef96dd7c5962361d
Lorena Silva <lorenarsdasilva@gmail.com>
2021-11-14 17:37:13 -0300

    Adicionando alguns exercicios novos

---
+++ src/Studyng/java/DateNova.java
@@ -0,0 +1,9 @@
+package Studyng.java;
+import java.util.Date;
+
+public class DateNova {
+    public static void main(String[] args) {
+        Date NovaData = new Date();
+        System.out.println(NovaData);
+    }
+}
